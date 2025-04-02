429fcaf774831b92e973557caa828f1e17eb99b1
hrishikeshdarshan <hrishikesh7165@gmail.com>
2025-04-01 23:26:51 -0400

    Fix typography imports

--- core/presentation/designsystem/src/main/java/com/hrishi/scribbledash/designsystem/Theme.kt
+++ core/presentation/designsystem/src/main/java/com/hrishi/scribbledash/designsystem/Theme.kt
@@ -2,6 +2,7 @@ package com.hrishi.scribbledash.designsystem
 
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
