82ae6d5b07ae86aa090bf0e5c419011291fc97ac
hrishikeshdarshan <hrishikesh7165@gmail.com>
2025-04-01 23:20:25 -0400

    Fixed typography error and build error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,36 +4,9 @@ plugins {
 
 android {
     namespace = "com.hrishi.scribbledash"
-    compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.hrishi.scribbledash"
-        minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-    buildFeatures {
-        compose = true
     }
 }
 
@@ -48,7 +21,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.material.icons.extended)
-    implementation(project(":core:presentation:designsystem"))
 
     // Testing dependencies
     testImplementation(libs.junit)
@@ -66,6 +38,7 @@ dependencies {
 
     implementation(libs.bundles.koin)
 
+    // Implement local modules
     implementation(projects.core.domain)
     implementation(projects.core.data)
     implementation(projects.core.presentation.designsystem)
