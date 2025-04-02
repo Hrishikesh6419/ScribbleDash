bbff778ce25be781616b751b61e651f6b5f7e69b
hrishikeshdarshan <hrishikesh7165@gmail.com>
2025-04-01 22:28:04 -0400

    Added colors in the App

--- app/src/main/java/com/hrishi/scribbledash/ui/theme/Color.kt
+++ app/src/main/java/com/hrishi/scribbledash/ui/theme/Color.kt
@@ -2,10 +2,20 @@ package com.hrishi.scribbledash.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Primary = Color(0xFF238CFF)
+val OnPrimary = Color(0xFFFFFFFF)
+val Secondary = Color(0xFFAB5CFA)
+val TertiaryContainer = Color(0xFFFA852C)
+val Error = Color(0xFFEF1242)
+val Background = Color(0xFFFEFAF6)
+val SurfaceHigh = Color(0xFFFFFFFF)
+val SurfaceLow = Color(0xFFEEE7E0)
+val SurfaceLowest = Color(0xFFE1D5CA)
+val OnBackground = Color(0xFF514437)
+val OnSurface = Color(0xFFA5978A)
+val OnSurfaceVariant = Color(0xFFF6F1EC)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+object ExtendedColors {
+    val Success = Color(0xFF0DD280)
+    val OnBackgroundVariant = Color(0xFF7F7163)
+}
--- app/src/main/java/com/hrishi/scribbledash/ui/theme/Theme.kt
+++ app/src/main/java/com/hrishi/scribbledash/ui/theme/Theme.kt
@@ -1,57 +1,38 @@
 package com.hrishi.scribbledash.ui.theme
 
-import android.app.Activity
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+private val ColorScheme = lightColorScheme(
+    primary = Primary,
+    onPrimary = OnPrimary,
+    secondary = Secondary,
+    tertiaryContainer = TertiaryContainer,
+    error = Error,
+    background = Background,
+    onBackground = OnBackground,
+    surfaceContainerHigh = SurfaceHigh,
+    surfaceContainerLow = SurfaceLow,
+    surfaceContainerLowest = SurfaceLowest,
+    onSurface = OnSurface,
+    onSurfaceVariant = OnSurfaceVariant
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+val ColorScheme.success: Color
+    get() = ExtendedColors.Success
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+val ColorScheme.onBackgroundVariant: Color
+    get() = ExtendedColors.OnBackgroundVariant
 
 @Composable
 fun ScribbleDashTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = ColorScheme,
         typography = Typography,
         content = content
     )
