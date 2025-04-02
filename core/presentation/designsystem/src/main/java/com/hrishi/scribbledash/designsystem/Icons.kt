d1d43da82c7b3ff6022ae43c511eb665e470f6e4
hrishikeshdarshan <hrishikesh7165@gmail.com>
2025-04-02 00:04:47 -0400

    Added Icons to the project

---
+++ core/presentation/designsystem/src/main/java/com/hrishi/scribbledash/designsystem/Icons.kt
@@ -0,0 +1,30 @@
+package com.hrishi.scribbledash.designsystem
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import com.scribbledash.presentation.designsystem.R
+
+val BackIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.ic_back)
+
+val ChartIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.ic_chart)
+
+val CloseIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.ic_close)
+
+val HomeIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.ic_home)
+
+val RedoIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.ic_redo)
+
+val UndoIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.ic_undo)
