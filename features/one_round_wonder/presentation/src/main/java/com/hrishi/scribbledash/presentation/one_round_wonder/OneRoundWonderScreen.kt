6684400f87f6b8c359d013e42fc2636de50fea2c
hrishikeshdarshan <hrishikesh7165@gmail.com>
2025-04-04 01:18:19 -0400

    Update interaction source

--- features/one_round_wonder/presentation/src/main/java/com/hrishi/scribbledash/presentation/one_round_wonder/OneRoundWonderScreen.kt
+++ features/one_round_wonder/presentation/src/main/java/com/hrishi/scribbledash/presentation/one_round_wonder/OneRoundWonderScreen.kt
@@ -2,6 +2,7 @@ package com.hrishi.scribbledash.presentation.one_round_wonder
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -21,6 +22,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -180,7 +182,10 @@ private fun DifficultySettingItem(
 ) {
     Column(
         modifier = modifier
-            .clickable { onClick(difficulty) },
+            .clickable(
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() }
+            ) { onClick(difficulty) },
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Icon(
