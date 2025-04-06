32de25f8d9df61db1d70f38fd7fd301f8264d216
hrishikeshdarshan <hrishikesh7165@gmail.com>
2025-04-06 03:03:09 -0400

    Added pseudo logo to the App

--- app/src/main/java/com/hrishi/scribbledash/MainActivity.kt
+++ app/src/main/java/com/hrishi/scribbledash/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.hrishi.scribbledash.designsystem.ScribbleDashTheme
 import com.hrishi.scribbledash.navigation.NavigationRoot
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
